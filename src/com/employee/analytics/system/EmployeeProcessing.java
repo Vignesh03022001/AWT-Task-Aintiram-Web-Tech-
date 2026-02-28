dc19adbef6b29cd6e9c0c30c88629e95c63b3c9f
Shri Vignesh <shrivigneshprasanna@gmail.com>
2026-02-28 23:41:14 +0530

    New Questions And Problems Done

--- src/com/employee/analytics/system/EmployeeProcessing.java
+++ src/com/employee/analytics/system/EmployeeProcessing.java
@@ -1,49 +1,61 @@
 package com.employee.analytics.system;
 
+import javax.swing.text.DateFormatter;
 import java.io.File;
 import java.io.FileReader;
+import java.text.spi.DateFormatProvider;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class EmployeeProcessing {
 
-	public static void main(String args[]) {
-		try {
-			File file = new File("employeeDetails.csv");
-			FileReader reader = new FileReader(file);
-			int character;
-			StringBuilder str = new StringBuilder();
-			while((character = reader.read())>0) {
-				str.append((char)character);
-			}
-//			System.out.println(str.toString());
-			convertStrAsList(str.toString());
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void convertStrAsList(String str) {
-		String[] eachLine = str.split("\n");
-		List<Employee> empList = new ArrayList<>();
-//		System.out.println(Arrays.toString(eachLine));
-		System.out.println(eachLine[3]);
-		for(String s : eachLine) {
-			String [] employeeData = s.split(",");
-//			System.out.println(Arrays.toString(employeeData));
-			int salary = Integer.parseInt(employeeData[3]);
-			String [] dateParts = employeeData[4].split("-");
-			int year = Integer.parseInt(dateParts[2]);
-			int month = Integer.parseInt(dateParts[1]);
-			int day = Integer.parseInt(dateParts[0]);
-			LocalDate date = LocalDate.of(year,month,day);
-			Employee e = new Employee(employeeData[0], employeeData[1], employeeData[2], salary, date, employeeData[4]);
-			empList.add(e);
-		}
-		
-		for(Employee e:empList) {
-//			System.out.println(e.toString());
-		}
-	}
+    public static void main(String args[]) {
+        try {
+            File file = new File("employeeDetails.csv");
+            FileReader reader = new FileReader(file);
+            int character;
+            StringBuilder str = new StringBuilder();
+            while ((character = reader.read()) > 0) {
+                str.append((char) character);
+            }
+            convertStrAsList(str.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void convertStrAsList(String str) {
+        String[] eachLine = str.split("\n");
+        List<Employee> empList = new ArrayList<>();
+        for (String s : eachLine) {
+            String[] employeeData = s.split(",");
+            int salary = 0;
+            if (!employeeData[3].isBlank()) salary = Integer.parseInt(employeeData[3]);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+            LocalDate date = LocalDate.parse(employeeData[4],formatter);
+            Employee e = new Employee(employeeData[0], employeeData[1], employeeData[2], salary, date, employeeData[4]);
+            empList.add(e);
+        }
+
+        for (Employee e : empList) {
+            //Find and print the names of all employees who work in the "IT" department.
+            if (e.getDept().equalsIgnoreCase("IT"))
+                System.out.println(e.getName());
+            if (e.getSalary() > 70000) System.out.println(e.getName() + "--" + e.getSalary());
+        }
+        int companyTotalSalaryPaid= empList.stream().mapToInt(Employee::getSalary).sum();
+        System.out.println("Company's Total Cost for Salary -->"+companyTotalSalaryPaid);
+        Optional<Employee> maxSalariedEmployee = empList.stream().max(Comparator.comparingInt(Employee::getSalary));
+        maxSalariedEmployee.ifPresent(e->{
+            System.out.println("Max Salaried Employee -->"+e.toString());
+        });
+        List<Employee> employeesJoinedBefore2022 = empList.stream().filter(e->{
+            LocalDate dt = e.getDoj();
+            return dt.getYear() < 2021;
+        }).toList();
+        System.out.println("Employee Joined Before 2022 -->>"+ employeesJoinedBefore2022);
+    }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,3 @@
 module AWTProgramingRound {
+    requires java.desktop;
 }
